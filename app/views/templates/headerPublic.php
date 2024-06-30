8ec1c5e814a83a52a8795ab268bd5f23f43d2baf
Kalen <116669445+kaleper@users.noreply.github.com>
2024-06-30 21:53:36 +0000

    Add visible navbar to headerPublic
    Give users who are inauthenticated a navigation bar that keeps the site's brand visible

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -17,3 +17,19 @@ if (isset($_SESSION['auth']) == 1) {
         <meta name="mobile-web-app-capable" content="yes">
 </head>
 <body>
+
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/home">Kalen's Site</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item"> 
+                <!-- Add nav items here if I decide it's needed -->
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
