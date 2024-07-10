8752ef355c5664998e5b8319bdbcb26c95e51fce
Kalen <116669445+kaleper@users.noreply.github.com>
2024-07-10 22:09:44 +0000

    Add bootstrap component, 'carousel'
    Carousel displays photos with slides showing information on what the site can provide.

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,8 +4,19 @@
 <style>
   <?php include "app/views/css/home.css"?>
 </style>
-<link rel="stylesheet" href="app/views/css/home.css"
+<script>
+    const toastTrigger = document.getElementById('liveToastBtn')
+    const toastLiveExample = document.getElementById('liveToast')
 
+    if (toastTrigger) {
+      const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+      toastTrigger.addEventListener('click', () => {
+        toastBootstrap.show()
+      })
+    }
+</script>
+<link rel="stylesheet" href="app/views/css/home.css"
+    
     <div class="container main">
         <div class="row mt-4">
             <div class="col-lg-12">
@@ -23,6 +34,48 @@
                         } 
                     ?> 
                 <p id="date" class= "text-center"> <span id ="date-label ">Today's Date:</span> <?= date("F jS, Y"); ?></p>
+
+                <div class="row justify-content-center">
+                    <div id="carousel" class="carousel carousel-dark slide" style="max-width: 800px;">
+                      <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                      </div>
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img src="app/views/resources/images/sticky_note.jpg" class="d-block w-100" alt="Sticky Note">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h5>Set Reminders</h5>
+                            <p>Forgetting things? Add reminders to keep your life organized.</p>
+                          </div>
+                        </div>
+                        <div class="carousel-item">
+                          <img src="app/views/resources/images/eraser.jpg" class="d-block w-100" alt="Eraser">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h5>Update and Delete Reminders</h5>
+                            <p>Make changes to reminders, check reminders off, and delete them as you finish tasks</p>
+                          </div>
+                        </div>
+                        <div class="carousel-item">
+                          <img src="app/views/resources/images/reports.jpg" class="d-block w-100" alt="Reports">
+                          <div class="carousel-caption d-none d-md-block">
+                            <h5>View Reports</h5>
+                            <p>As an administrator of this site, view all reminders and login history of users.</p>
+                          </div>
+                        </div>
+                      </div>
+                      <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                      </button>
+                      <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                      </button>
+                    </div>
+                </div>
+        
             </div>
         </div>
     </div>
